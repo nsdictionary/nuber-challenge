6b927ebfea014b4399a97c54b98ae82f619333d2
sam <3161728@gmail.com>
2021-01-05 09:17:02 +0900

    Add podcasts routes

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,22 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  // check available routes for test
+  const server = app.getHttpServer();
+  const router = server._events.request._router;
+  const availableRoutes: [] = router.stack
+    .map((layer) => {
+      if (layer.route) {
+        return {
+          route: {
+            path: layer.route?.path,
+            method: layer.route?.stack[0].method,
+          },
+        };
+      }
+    })
+    .filter((item) => item !== undefined);
+  console.log(availableRoutes);
 }
 bootstrap();
--- src/podcasts/podcasts.controller.ts
+++ src/podcasts/podcasts.controller.ts
@@ -1,10 +1,55 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('podcasts')
 export class PodcastsController {
-
   @Get()
-  getHello(): string {
-    return 'Hello';
+  getAll(): string {
+    return 'This will return all podcasts';
+  }
+
+  @Post()
+  create(): string {
+    return 'This will create a podcast';
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') podcastId: string): string {
+    return `This will return one podcast with the id: ${podcastId}`;
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') podcastId: string): string {
+    return `This will patch a podcast with the id: ${podcastId}`;
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') podcastId: string): string {
+    return `This will delete a podcast with the id: ${podcastId}`;
+  }
+
+  @Get('/:id/episodes')
+  getEpisode(@Param('id') podcastId: string): string {
+    return 'This will return episodes';
+  }
+
+  @Post('/:id/episodes')
+  createEpisode(@Param('id') podcastId: string): string {
+    return 'This will create a episode';
+  }
+
+  @Patch('/:id/episodes/:episodeId')
+  patchEpisode(
+    @Param('id') podcastId: string,
+    @Param('episodeId') episodeId: string,
+  ): string {
+    return 'This will patch a episode';
+  }
+
+  @Delete('/:id/episodes/:episodeId')
+  deleteEpisode(
+    @Param('id') podcastId: string,
+    @Param('episodeId') episodeId: string,
+  ): string {
+    return 'This will delete a episode';
   }
 }
